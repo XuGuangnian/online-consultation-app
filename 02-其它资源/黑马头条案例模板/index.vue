7c9faffe0ade8f1f8b39e506d1904b9c50320630
lamphc <lamphc@live.com>
2022-09-14 10:02:17 +0800

    init

---
+++ 02-其它资源/黑马头条案例模板/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import './styles/index.css'
+import ChannelNav from './components/ChannelNav.vue';
+import ArticleList from './components/ArticleList.vue'
+</script>
+  
+<template>
+  <ChannelNav />
+  <ArticleList />
+</template>

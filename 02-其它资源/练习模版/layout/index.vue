3c04e978103c595ad31d61adf0aeff03e592e81a
lamphc <lamphc@live.com>
2022-09-20 11:56:06 +0800

    开发模版

---
+++ 02-其它资源/练习模版/layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout-page">
+    <van-tabbar route>
+      <van-tabbar-item to="/home">首页</van-tabbar-item>
+      <van-tabbar-item to="/article">健康百科</van-tabbar-item>
+      <van-tabbar-item to="/notify">消息中心</van-tabbar-item>
+      <van-tabbar-item to="/user">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ 02-其它资源/练习模版/room/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import RoomStatus from './components/RoomStatus.vue'
+import RoomAction from './components/RoomAction.vue'
+import RoomMessage from './components/RoomMessage.vue'
+</script>
+
+<template>
+  <div class="room-page">
+    <cp-nav-bar title="医生问诊室" />
+    <!-- 1. 顶部订单状态 -->
+    <room-status />
+    <RoomMessage />
+    <!-- 2. 底部订单操作栏 -->
+    <room-action />
+  </div>
+</template>

7adb0f6fa3e78376139a4600dbd012953b1df1db
lamphc <lamphc@live.com>
2022-11-05 11:41:10 +0800

    类型注解

---
+++ codex/typescript-hello/1-类型注解.ts
@@ -0,0 +1,17 @@
+/**
+ * 高频使用
+ * 作用：给变量添加类型约束
+ * 语法=》变量名:类型
+ */
+let hello: string = 'hi'
+
+hello = 'hi vue3'
+// hello = 123
+
+let abc: boolean = true
+
+// abc = 123
+abc = false
+
+let test: null = null
+test = null

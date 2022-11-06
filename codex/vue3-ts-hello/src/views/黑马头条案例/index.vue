c586218269f83f44b420a731256626278fd9e22a
lamphc <lamphc@live.com>
2022-11-06 16:13:44 +0800

    案例模版

---
+++ codex/vue3-ts-hello/src/views/黑马头条案例/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import './styles/index.css'
+import ChannelNav from './components/ChannelNav.vue'
+import ArticleList from './components/ArticleList.vue'
+</script>
+
+<template>
+  <!-- 1. 上边频道菜单组件 -->
+  <ChannelNav />
+  <!-- 2. 新闻列表 -->
+  <ArticleList />
+</template>

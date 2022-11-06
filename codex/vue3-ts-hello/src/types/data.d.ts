3b58c91762fe278b624a5fbcf0c0e8f30842ba11
lamphc <lamphc@live.com>
2022-11-06 16:36:58 +0800

    频道渲染

--- codex/vue3-ts-hello/src/types/data.d.ts
+++ codex/vue3-ts-hello/src/types/data.d.ts
@@ -8,3 +8,15 @@ export type Data = {
   name: string
   age?: number
 }
+// 案例类型
+// 后台返回数据类型
+export type ChannelItem = {
+  id: number
+  name: string
+}
+export type ResData = {
+  data: {
+    channels: ChannelItem[]
+  }
+  message: string
+}
--- codex/vue3-ts-hello/src/views/黑马头条案例/components/ChannelNav.vue
+++ codex/vue3-ts-hello/src/views/黑马头条案例/components/ChannelNav.vue
@@ -1,10 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+// 导入类型
+import { ChannelItem, ResData } from '../../../types/data'
+
+// 定义响应变量
+const list = ref<ChannelItem[]>([])
+
+// 获取频道数据
+const getChannels = async () => {
+  const res = await axios.get<ResData>('http://geek.itheima.net/v1_0/channels')
+  console.log('频道数据：', res.data.data.channels)
+  list.value = res.data.data.channels
+}
+
+onMounted(() => {
+  getChannels()
+})
+</script>
 
 <template>
   <div class="channel-nav">
     <nav class="list">
-      <a class="item" :class="{ active: i === 0 }" href="javascript:;" v-for="(item, i) in 10" :key="i">
-        推荐{{ item }}
+      <a
+        class="item"
+        :class="{ active: i === 0 }"
+        href="javascript:;"
+        v-for="(item, i) in list"
+        :key="item.id"
+      >
+        {{ item.name }}
       </a>
     </nav>
   </div>

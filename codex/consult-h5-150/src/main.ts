b9a8f92c82ccb186f65bebdfd7f9a3dfc9554343
lamphc <lamphc@live.com>
2022-11-08 15:21:50 +0800

    路由代码解析

--- codex/consult-h5-150/src/main.ts
+++ codex/consult-h5-150/src/main.ts
@@ -4,11 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
+// 1. 创建vue根实例
 const app = createApp(App)
 
+// 2. 注册pinia和router
 app.use(createPinia())
 app.use(router)
-
+// 3. 挂载渲染App根组件
 app.mount('#app')
--- codex/consult-h5-150/vite.config.ts
+++ codex/consult-h5-150/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base:'/服务器中放置打包代码的目录名/'
   plugins: [
     vue({
       reactivityTransform: true

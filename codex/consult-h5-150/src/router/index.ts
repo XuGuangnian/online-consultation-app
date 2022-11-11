1a79b44c286582bf0f20a12655baaad41efd2a54
lamphc <lamphc@live.com>
2022-11-11 15:06:29 +0800

    09-给路由meta添加类型和抽离routes配置

--- codex/consult-h5-150/src/router/index.ts
+++ codex/consult-h5-150/src/router/index.ts
@@ -1,37 +1,7 @@
 import { useUserStore } from '@/stores'
 import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
-
-// import Test from '@/views/test/index.vue'
-const Test = () => import('@/views/test/index.vue')
-
-// 路由配置数组
-const routes = [
-  // 一级路由=》挂载点router-view放到App.vue根组件
-  {
-    path: '/login',
-    component: () => import('@/views/login/index.vue')
-  },
-  // 测试页面
-  {
-    path: '/test',
-    name: 'test',
-    component: Test
-  },
-  // tabBar页面
-  {
-    // 父路由：layout公共布局页面
-    path: '/',
-    component: () => import('@/views/layout/index.vue'),
-    children: [
-      // 二级路由：挂载点router-view放到父路由指定位置
-      // 子路由：首页、健康百科、消息中心、我的
-      { path: '/home', component: () => import('@/views/home/index.vue') },
-      { path: '/article', component: () => import('@/views/article/index.vue') },
-      { path: '/notify', component: () => import('@/views/notify/index.vue') },
-      { path: '/user', component: () => import('@/views/user/index.vue') }
-    ]
-  }
-]
+// 导入路由配置
+import routes from './routes'
 
 // 1. 创建路由实例
 // 之前vue2：是通过 new Router创建路由实例
@@ -54,6 +24,9 @@ const router = createRouter({
  * vue3: to 去哪 from 从哪来
  */
 router.beforeEach((to) => {
+  // console.log('正在访问页面：', to)
+  // 给页面动态添加title
+  document.title = `在线问诊-${to.meta.title || '页面没有title'}`
   /**
    * 页面访问拦截:
    * 根据是否有token，决定用户是否可以访问to页面
@@ -63,6 +36,8 @@ router.beforeEach((to) => {
   const store = useUserStore()
   // 说明：['/login', '/other']不需要登录就可以访问=》vip用户
   // 如果没有token，同时不在vip里边=》重新登录
+  // ts类型机制：类型检查会判断复杂类型的值是否存在，在代码执行之前
+  // 就把一些隐含的类型错误暴漏出来
   if (!store.user.token && !['/login', '/other'].includes(to.path)) return '/login'
 })
 

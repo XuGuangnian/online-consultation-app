21543fb52155c9458783b79406e35e1a0702eec6
lamphc <lamphc@live.com>
2022-11-11 10:59:40 +0800

    tabbar配置

--- codex/consult-h5-150/src/router/index.ts
+++ codex/consult-h5-150/src/router/index.ts
@@ -5,6 +5,7 @@ const Test = () => import('@/views/test/index.vue')
 
 // 路由配置数组
 const routes = [
+  // 一级路由=》挂载点router-view放到App.vue根组件
   {
     path: '/login',
     component: () => import('@/views/login/index.vue')
@@ -14,6 +15,20 @@ const routes = [
     path: '/test',
     name: 'test',
     component: Test
+  },
+  // tabBar页面
+  {
+    // 父路由：layout公共布局页面
+    path: '/',
+    component: () => import('@/views/layout/index.vue'),
+    children: [
+      // 二级路由：挂载点router-view放到父路由指定位置
+      // 子路由：首页、健康百科、消息中心、我的
+      { path: '/home', component: () => import('@/views/home/index.vue') },
+      { path: '/article', component: () => import('@/views/article/index.vue') },
+      { path: '/notify', component: () => import('@/views/notify/index.vue') },
+      { path: '/user', component: () => import('@/views/user/index.vue') }
+    ]
   }
 ]
 
---
+++ codex/consult-h5-150/src/views/layout/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout-page">
+    <!-- 上部：显示子路由页面 -->
+    <router-view />
+
+    <!-- 底部：tabBar组件 
+    route 开启路由模式
+    -->
+    <van-tabbar route>
+      <van-tabbar-item to="/home">首页</van-tabbar-item>
+      <van-tabbar-item to="/article">健康百科</van-tabbar-item>
+      <van-tabbar-item to="/notify">消息中心</van-tabbar-item>
+      <van-tabbar-item to="/user">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

bc1d970fd95ec8b884ce4e973a621eaf8787660b
lamphc <lamphc@live.com>
2022-11-11 11:39:40 +0800

    06-tabbar样式自定义

--- codex/consult-h5-150/src/views/layout/index.vue
+++ codex/consult-h5-150/src/views/layout/index.vue
@@ -7,14 +7,49 @@
     route 开启路由模式
     -->
     <van-tabbar route>
-      <van-tabbar-item to="/home">首页</van-tabbar-item>
-      <van-tabbar-item to="/article">健康百科</van-tabbar-item>
-      <van-tabbar-item to="/notify">消息中心</van-tabbar-item>
-      <van-tabbar-item to="/user">我的</van-tabbar-item>
+      <van-tabbar-item to="/home">
+        首页
+        <!-- 作用域插槽:
+          1. 自定义图标
+          2. active获取当前菜单是否选中：true 选中(显示index-active.sng) | false 未选中(显示index-default.svg)
+         -->
+        <template #icon="{ active }">
+          <cp-icon :name="`home-index-${active ? 'active' : 'default'}`"></cp-icon>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/article">
+        健康百科
+        <template #icon="{ active }">
+          <cp-icon :name="`home-article-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/notify">
+        消息中心
+        <template #icon="{ active }">
+          <cp-icon :name="`home-notice-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/user">
+        我的
+        <template #icon="{ active }">
+          <cp-icon :name="`home-mine-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+// 自定义tabbar样式
+::v-deep() {
+  .van-tabbar-item__icon {
+    font-size: 18px;
+  }
+  .van-tabbar-item__text {
+    font-size: 14px;
+    // color: red;
+  }
+}
+</style>

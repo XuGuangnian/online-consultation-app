a71269f2fa2817bccab2acae8e4579bbd9ac0de6
lamphc <lamphc@live.com>
2022-11-12 17:33:01 +0800

    16-首页模块-知识列表上拉加载更多实现

--- codex/consult-h5-150/src/views/home/components/KnowledgeList.vue
+++ codex/consult-h5-150/src/views/home/components/KnowledgeList.vue
@@ -1,30 +1,51 @@
 <script setup lang="ts">
 import KnowledgeCard from './KnowledgeCard.vue'
 import { ref } from 'vue'
+import type { KnowledgeType, KnowledgeList } from '@/types/consult'
+import { getKnowledgePage } from '@/api/consult'
+
+// 接收type（文章类型）
+const props = defineProps<{
+  type: KnowledgeType
+}>()
 
 // 列表数据
-const list = ref<number[]>([])
-// 列表加载状态：true 加载中 | false 加载完成 （多次触发）
+const list = ref<KnowledgeList>([])
+// 列表加载状态：true 加载中 | false 加载完成，关闭loading效果 （多次触发）
 const loading = ref(false)
-// 控制列表数据是否加载完成：false 还有数据 | true 数据已经加载完毕
+// 控制列表数据是否加载完成：
+// false 还有数据 | true 数据已经加载完毕（再次触底不会执行onLoad）
 const finished = ref(false)
+
+// 请求的分页参数
+const params = {
+  type: props.type, // 频道类型
+  current: 1, // 请求的页码
+  pageSize: 10 // 每页多少条数据
+}
 // 加载数据的函数：1. 页面打开后默认执行一次 2. 每次用户滚动到底部会再次执行
-const onLoad = () => {
+const onLoad = async () => {
   // 异步更新数据
   // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-  setTimeout(() => {
-    for (let i = 0; i < 10; i++) {
-      list.value.push(list.value.length + 1)
-    }
+  const { data } = await getKnowledgePage(params)
+  // 把当前页数据追加到list列表中
+  list.value.push(...data.rows)
+  // 加载状态结束
+  loading.value = false
 
-    // 加载状态结束
-    loading.value = false
-
-    // 数据全部加载完成
-    if (list.value.length >= 40) {
-      finished.value = true
-    }
-  }, 1000)
+  // 数据全部加载完成
+  /**
+   * 判断列表的数据是否加载完成了？
+   * 1. list长度 === total 数据总条数
+   * 2. current === pageTotal 总页数
+   */
+  if (list.value.length === data.total) {
+    // 数据加载完了
+    finished.value = true
+  } else {
+    // 数据没有加载完？把页面加一
+    params.current++
+  }
 }
 </script>
 
@@ -38,7 +59,7 @@ const onLoad = () => {
     >
       <!-- 列表数据 -->
       <!-- 知识列表item -->
-      <knowledge-card v-for="i in 5" :key="i"></knowledge-card>
+      <knowledge-card v-for="item in list" :key="item.id" :item="item"></knowledge-card>
     </van-list>
   </div>
 </template>

2ebc7150657288b6aa35c3094fc2ed9e954ecc50
lamphc <lamphc@live.com>
2022-11-14 09:53:28 +0800

    04-首页模块-推荐关注医生-展示

--- codex/consult-h5-150/src/api/consult.ts
+++ codex/consult-h5-150/src/api/consult.ts
@@ -1,5 +1,8 @@
-import type { KnowledgePage, KnowledgeParams } from '@/types/consult'
+import type { KnowledgePage, KnowledgeParams, PageParams, DoctorPage } from '@/types/consult'
 import { request } from '@/utils/request'
 // 获取健康知识列表
 export const getKnowledgePage = (params: KnowledgeParams) =>
   request.get<KnowledgePage>('/patient/home/knowledge', { params })
+// 获取关注医生列表
+export const getDoctorPage = (params: PageParams) =>
+  request.get<DoctorPage>('/home/page/doc', { params })
--- codex/consult-h5-150/src/views/home/components/FollowDoctor.vue
+++ codex/consult-h5-150/src/views/home/components/FollowDoctor.vue
@@ -1,16 +1,60 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import DoctorCard from './DoctorCard.vue'
+import { useWindowSize } from '@vueuse/core'
+import { getDoctorPage } from '@/api/consult'
+import type { DoctorList } from '@/types/consult'
+// 1. 原生js方式
+// const width = ref(375)
+// onMounted(() => {
+//   // 通过window.innerWidth获取手机屏幕宽度
+//   width.value = window.innerWidth
+//   // 屏幕变化，宽度需要重新计算=》window添加resize事件
+//   window.addEventListener('resize', () => {
+//     width.value = window.innerWidth
+//   })
+// })
+// 2. 使用@vue/core
+// 获取手机屏幕宽度
+const { width } = useWindowSize()
+
+// 3. 获取关注医生列表数据
+const list = ref<DoctorList>([])
+const getList = async () => {
+  const { data } = await getDoctorPage({ current: 1, pageSize: 5 })
+  console.log('医生列表：', data)
+  // 默认展示5个医生
+  list.value = data.rows
+}
+onMounted(() => {
+  getList()
+})
 </script>
 
 <template>
   <div class="follow-doctor">
+    <!-- 1. 头部 -->
     <div className="head">
       <p>推荐关注</p>
       <a href="javascript:;"> 查看更多<i class="van-icon van-icon-arrow" /></a>
     </div>
+    <!-- 2. 医生列表 -->
     <div class="body">
-      <!-- swipe 组件 -->
-      <doctor-card />
+      <!-- swipe 组件 
+      width指定单个item的宽度=》问题：150写死的
+      解决：基准比例(150 / 375) * 当前屏幕宽度
+      -->
+      <van-swipe
+        :autoplay="3000"
+        :width="(150 / 375) * width"
+        :loop="false"
+        :show-indicators="false"
+      >
+        <van-swipe-item v-for="item in list" :key="item.id">
+          <!-- 单个医生的信息介绍 -->
+          <doctor-card :item="item" />
+        </van-swipe-item>
+      </van-swipe>
     </div>
   </div>
 </template>

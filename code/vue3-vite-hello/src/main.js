88f3482c00b39c0e52d63f09fba05b127920d82e
lamphc <lamphc@live.com>
2023-04-06 18:16:43 +0800

    20-今日作业说明

--- code/vue3-vite-hello/src/main.js
+++ code/vue3-vite-hello/src/main.js
@@ -11,7 +11,13 @@ import { createApp } from 'vue'
 // import App from './views/data变量处理/computed函数.vue'
 // import App from './views/data变量处理/watch函数.vue'
 // import App from './views/生命周期/index.vue'
-import App from './views/模版ref/index.vue'
+// import App from './views/模版ref/index.vue'
+import App from './views/案例/index.vue'
+
+// 引入饿了吗组件库
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 
 
@@ -23,4 +29,5 @@ import App from './views/模版ref/index.vue'
 
 
 // 2. 创建根实例，挂载显示根组件
-createApp(App).mount('#app')
+// 注册组件库
+createApp(App).use(ElementPlus).mount('#app')
---
+++ code/vue3-vite-hello/src/views/案例/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>案例作业</h1>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>

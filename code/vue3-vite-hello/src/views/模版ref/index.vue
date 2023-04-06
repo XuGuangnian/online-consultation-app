1fd4ceb0d49d59a5080ce4a8fbcffaa2b12933b4
lamphc <lamphc@live.com>
2023-04-06 17:51:50 +0800

    模版ref-获取组件实例

--- code/vue3-vite-hello/src/views/模版ref/index.vue
+++ code/vue3-vite-hello/src/views/模版ref/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h1 ref="h1Ref">模版ref使用</h1>
+    <hr />
+    <!-- <ElForm></ElForm> -->
+    <el-form ref="formRef"></el-form>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+// 导入组件 => 导入即注册=》元素名为导入的组件的名字
+import ElForm from './son.vue'
 /**
  * 模版ref使用
  * 1-获取dom对象
@@ -15,13 +20,24 @@ import { ref, onMounted } from 'vue'
     3. 使用 => `hRef.value`
  * 2-获取组件实例   
  */
+// 1-获取dom对象 (了解)
 const h1Ref = ref()
 // 问题：能不能获取h1 dom? => 不能
 console.log('setup-获取h1 dom：', h1Ref.value)
 onMounted(() => {
   // 组件挂载渲染完了执行
+  // vue2: this.$refs.ref值 ===  vue3: h1Ref.value
   console.log('onMounted-获取h1 dom：', h1Ref.value)
   h1Ref.value.style.border = '10px solid pink'
+})
+
+// 2-获取组件实例(掌握了解)
+const formRef = ref()
+onMounted(() => {
+  // vue2: this.$refs.formRef.validate() => 说明：this.$refs.formRef等价于son.vue的this
+  console.log('获取form组件实例：', formRef.value)
+  formRef.value.validate()
+  formRef.value.meng()
 })
 </script>
 
---
+++ code/vue3-vite-hello/src/views/模版ref/son.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>表单子组件</h2>
+  </div>
+</template>
+
+<script setup>
+// vue2: 在methods里边定义方法validate
+// 兜底检验方法=>问题：vue3默认情况下定义的方法外部不能直接访问
+const validate = () => {
+  console.log('兜底检验方法')
+}
+const test = () => {
+  console.log(123)
+}
+// 解决：通过defineExpose全局宏函数，对外提供方法
+defineExpose({
+  // validate:validate
+  validate, // 简写
+  meng: test,
+})
+</script>
+
+<style lang="scss" scoped></style>

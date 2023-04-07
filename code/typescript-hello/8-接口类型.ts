83af6d5efb7662bfeaab57956981f50eb451b673
lamphc <lamphc@live.com>
2023-04-07 17:11:00 +0800

    接口类型的继承

--- code/typescript-hello/8-接口类型.ts
+++ code/typescript-hello/8-接口类型.ts
@@ -2,7 +2,7 @@
  * 语法：interface 接口类型名字 {key1:type1; key2?:type2...}
  * 作用：给对象定义类型
  */
-
+// 1. 接口类型基本使用
 interface Student {
   name: string
   age: number
@@ -14,7 +14,6 @@ interface Student {
 }
 
 let stu: Student
-
 stu = {
   name: '小红',
   age: 18,
@@ -22,6 +21,35 @@ stu = {
   cj: 100,
   height: 180
 }
+
+// 2. 接口类型的继承
+interface Person {
+  name: string
+  age?: number
+  say: () => void
+}
+// 需求：Teacher接口类型想要使用Person接口类型中的属性=》继承
+interface Teacher extends Person {
+  // name: string
+  // age: number
+  // say: () => void
+  wirteCode: () => void
+}
+
+let twang: Teacher = {
+  name: '王猛',
+  // age:
+  say() {
+
+  },
+  wirteCode() {
+
+  },
+}
+
+
+
+
 
 
 

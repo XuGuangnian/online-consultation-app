d5ce2ce9dd15823742c5b1c176afc5b327ac4cc3
lamphc <lamphc@live.com>
2023-04-09 16:28:16 +0800

    事件处理与Typescript

--- code/vue-ts-hello/src/main.ts
+++ code/vue-ts-hello/src/main.ts
@@ -5,7 +5,9 @@ import { createApp } from 'vue'
 // 导入自己练习页面
 // import App from './views/data变量定义/ref函数.vue'
 // import App from './views/data变量定义/reactive函数.vue'
-import App from './views/组件通信/index.vue'
+// import App from './views/组件通信/index.vue'
+import App from './views/事件处理/index.vue'
+
 
 
 
---
+++ code/vue-ts-hello/src/views/事件处理/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- 说明：$event获取事件对象,可以查看事件对象的类型 -->
+    <input type="text" @change="getVal($event)" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const getVal = (e: Event) => {
+  console.log('事件对象：', e)
+  // 怎么查看某个html元素类型？=》document.querySelector('元素名').__proto__
+  console.log('输入框输入的值：', (e.target as HTMLInputElement).value)
+}
+</script>
+
+<style scoped></style>

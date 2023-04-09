98e2ebd21fea5ccfe2b595ba55e80f5777c8d35d
lamphc <lamphc@live.com>
2023-04-09 18:28:16 +0800

    25-TS黑马头条案例-预习

---
+++ code/vue-ts-hello/src/views/黑马头条/components/ArticleList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="article-list">
+    <div class="article-item" v-for="i in 10" :key="i">
+      <p class="title">美国强行关闭33家伊朗网站 伊总统办公室警告：不利于伊核谈判</p>
+      <img class="img" src="https://inews.gtimg.com/newsapp_ls/0/13685285688_294195/0" alt="">
+      <img class="img" src="https://inews.gtimg.com/newsapp_ls/0/13685285688_294195/0" alt="">
+      <img class="img" src="https://inews.gtimg.com/newsapp_ls/0/13685285688_294195/0" alt="">
+      <div class="info">
+        <span>小兵张嘎</span>
+        <span>17评论</span>
+        <span>1天前</span>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ code/vue-ts-hello/src/views/黑马头条/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// 公共样式
+import './styles/index.css'
+// 菜单
+import ChannelNav from './components/ChannelNav.vue'
+// 文章列表
+import ArticleList from './components/ArticleList.vue'
+</script>
+
+<template>
+  <ChannelNav />
+  <ArticleList />
+</template>

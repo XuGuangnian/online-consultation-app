da5827c6b071d1fc4327b05270fb0a2dd854ef22
lamphc <lamphc@live.com>
2023-04-09 15:59:17 +0800

    defineEmits与Typescript

--- code/vue-ts-hello/src/views/组件通信/index.vue
+++ code/vue-ts-hello/src/views/组件通信/index.vue
@@ -2,7 +2,12 @@
   <div>
     <h1>父组件</h1>
     <hr />
-    <Son msg="123" />
+    <Son
+      :money="money"
+      msg="123"
+      @change-money="changeMoney"
+      @change-msg="changeMsg"
+    />
   </div>
 </template>
 
@@ -12,6 +17,20 @@ import Son from './son.vue'
 
 // 父传子
 const money = ref(1e5)
+
+// 子传父回调
+/**
+ *
+ * @param mky 子传父数据
+ */
+const changeMoney = (mky: number) => {
+  console.log('子传父：', mky)
+  money.value = mky
+}
+
+const changeMsg = (msg: string) => {
+  console.log('子传父-msg:', msg)
+}
 </script>
 
 <style scoped></style>
--- code/vue-ts-hello/src/views/组件通信/son.vue
+++ code/vue-ts-hello/src/views/组件通信/son.vue
@@ -17,6 +17,17 @@ const { money = 100 } = defineProps<{
 }>()
 
 console.log(money)
+
+// 子传父=> emit('事件名', data) 等价于 this.$emit('事件名', data)
+// const emit = defineEmits(['change-money'])
+// 子传父=> 使用ts定义类型=>给 emit函数定义类型
+const emit = defineEmits<{
+  (e: 'change-money', data: number): void // 自定义事件1
+  (e: 'change-msg', msg: string): void // 自定义事件2
+}>()
+emit('change-money', 2e6)
+
+emit('change-msg', '我是msg')
 </script>
 
 <style scoped></style>

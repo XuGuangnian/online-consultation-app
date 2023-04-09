3b35faa786370263c3a89b2c43bd543d1a7034a3
lamphc <lamphc@live.com>
2023-04-09 14:40:49 +0800

    reactive与TypeScript

---
+++ code/vue-ts-hello/src/views/data变量定义/reactive函数.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+// 1. 设置默认值了会自动推导类型=》不推荐
+const person = reactive({ name: '小红', age: 18 })
+
+// 2. 设置空对象作为默认值=> 推荐
+type Person = {
+  name: string
+  age: number
+}
+const person2 = reactive({} as Person)
+</script>
+
+<style scoped></style>
